d101a835f36133b0d22f9afaa1cc8df5f511ad82
권영재 <dwdjjj@naver.com>
2023-05-03 09:17:27 +0900

    first commit

---
+++ 0503_배포/1.varVSletVSconst.js
@@ -0,0 +1,47 @@
+// var VS let Scope Test
+function scope1() {
+  var x = 11;
+  if (true) {
+    var x = 99;
+    console.log(x);
+  }
+  console.log(x);
+}
+// scope1();
+
+function scope2() {
+  let x = 11;
+  if (true) {
+    let x = 99;
+    console.log(x);
+  }
+  console.log(x);
+}
+// scope2();
+
+function scope3() {
+  if (true) {
+    var x = 99;
+    console.log(x);
+  }
+  console.log(x);
+}
+// scope3();
+
+function scope4() {
+  let x = 99;
+  if (true) {
+    console.log(x);
+  }
+  console.log(x);
+}
+// scope4();
+
+function scope5() {
+  if (true) {
+    let x = 99;
+    console.log(x);
+  }
+  console.log(x);
+}
+// scope5();
