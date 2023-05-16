2f9e52f12041d15b1491bfe06e33357881b7db59
권영재 <dwdjjj@naver.com>
2023-05-16 11:26:51 +0900

    upload 0516 live practice

--- board-housedeal-todolist2/src/components/todo/TodoInput.vue
+++ board-housedeal-todolist2/src/components/todo/TodoInput.vue
@@ -1,34 +1,40 @@
 <template>
-  <b-container class="bv-example-row">
-    <b-row class="text-center">
-      <b-col>
-        <b-input-group prepend="할일" class="mt-3">
-          <b-form-input v-model.trim="todoTitle" @keypress.enter="registerTodo"></b-form-input>
-          <b-input-group-append>
-            <b-button variant="outline-success" @click="registerTodo">등록</b-button>
-          </b-input-group-append>
-        </b-input-group>
-      </b-col>
-    </b-row>
-  </b-container>
+    <b-container class="bv-example-row">
+        <b-row class="text-center">
+            <b-col>
+                <b-input-group prepend="할일" class="mt-3">
+                    <b-form-input v-model.trim="todoTitle" @keypress.enter="registerTodo"></b-form-input>
+                    <b-input-group-append>
+                        <b-button variant="outline-success" @click="registerTodo">등록</b-button>
+                    </b-input-group-append>
+                </b-input-group>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
-  name: "TodoInput",
-  data() {
-    return {
-      todoTitle: "",
-    };
-  },
-  methods: {
-    registerTodo() {
-      // const todoItem = {
-      //   title: this.todoTitle,
-      //   completed: false,
-      // };
+    name: "TodoInput",
+    data() {
+        return {
+            todoTitle: "",
+        };
     },
-  },
+    methods: {
+        ...mapActions(["createTodo"]),
+        registerTodo() {
+            const todoItem = {
+                title: this.todoTitle,
+                completed: false,
+            };
+            // this.$store.state.todos.push(todoItem);
+            // if (this.todoTitle) this.$store.commit("CREATE_TODO", todoItem);
+            // if (this.todoTitle) this.$store.dispatch("createTodo", todoItem);
+            if (this.todoTitle) this.createTodo(todoItem);
+        },
+    },
 };
 </script>
 
--- board-housedeal-todolist2/src/components/todo/TodoList.vue
+++ board-housedeal-todolist2/src/components/todo/TodoList.vue
@@ -1,22 +1,31 @@
 <template>
-  <b-container class="bv-example-row">
-    <b-row class="mt-4 mb-4 text-center">
-      <b-col>
-        <!-- <todo-list-item v-for="(todo, index) in todos" :key="index" :todo="todo" /> -->
-      </b-col>
-    </b-row>
-  </b-container>
+    <b-container class="bv-example-row">
+        <b-row class="mt-4 mb-4 text-center">
+            <b-col>
+                <todo-list-item v-for="(todo, index) in todos" :key="index" :todo="todo" />
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
-// import TodoListItem from "@/components/todo/TodoListItem";
+import { mapState } from "vuex";
+import TodoListItem from "@/components/todo/TodoListItem";
 
 export default {
-  name: "TodoList",
-  components: {
-    // TodoListItem,
-  },
-  computed: {},
+    name: "TodoList",
+    components: {
+        TodoListItem,
+    },
+    computed: {
+        // todos() {
+        //     return this.$store.state.todos;
+        // },
+        // ...mapState({
+        //   todos: "todos"
+        // }),
+        ...mapState(["todos"]),
+    },
 };
 </script>
 
