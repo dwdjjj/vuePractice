7f56f002570a74b4e007a4ad8088ae2b39449f92
권영재 <dwdjjj@naver.com>
2023-05-16 09:15:28 +0900

    upload 0516 live files

---
+++ board-housedeal-todolist2/src/components/todo/TodoHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-row class="mt-1 text-center">
+      <b-col> <h1>Todo it!</h1> </b-col>
+    </b-row>
+    <b-row class="mt-1 text-center">
+      <b-col>
+        <b-alert show variant="primary">할일 : 0</b-alert>
+      </b-col>
+      <b-col>
+        <b-alert show variant="success">진행 : 0</b-alert>
+      </b-col>
+      <b-col>
+        <b-alert show variant="danger">완료 : 0</b-alert>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "TodoHeader",
+  computed: {},
+};
+</script>
+
+<style></style>
---
+++ board-housedeal-todolist2/src/components/todo/TodoInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-row class="text-center">
+      <b-col>
+        <b-input-group prepend="할일" class="mt-3">
+          <b-form-input v-model.trim="todoTitle" @keypress.enter="registerTodo"></b-form-input>
+          <b-input-group-append>
+            <b-button variant="outline-success" @click="registerTodo">등록</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "TodoInput",
+  data() {
+    return {
+      todoTitle: "",
+    };
+  },
+  methods: {
+    registerTodo() {
+      // const todoItem = {
+      //   title: this.todoTitle,
+      //   completed: false,
+      // };
+    },
+  },
+};
+</script>
+
+<style></style>
